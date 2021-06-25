f490f3f7e3ccbd66c18362ce2abf3eb360ab22e6
Nuri Nawawi <nurihefez@gmail.com>
2021-06-25 13:40:53 +0700

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(e){
+	const options = {
+		settings: {
+			duration: 1500,
+		},
+		style: {
+			main: {
+				background: "pink",
+				color: "black",
+			},
+		},
+	};
+	iqwerty.toast.toast('Hello!', options);
+})
